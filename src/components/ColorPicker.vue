014177c16a27bf1888b88bb162208f2d0be31003
SteveMustanski <steve.mustanski@gmail.com>
2020-01-19 18:12:22 -0500

    added first test using TDD cycle

--- src/components/ColorPicker.vue
+++ src/components/ColorPicker.vue
@@ -1,9 +1,27 @@
 <template>
-  <div></div>
+  <div class="color-picker">
+    <ul class="swatches">
+      <li
+        :key="index"
+        v-for="(swatch, index) in swatches"
+        :style="{ background: `#${swatch}` }"
+        class="swatch"
+      ></li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    swatches: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+};
 </script>
 
 <style>
--- tests/unit/ColorPicker.spec.js
+++ tests/unit/ColorPicker.spec.js
@@ -1,6 +1,25 @@
-import { shallowMount } from '@vue/test-utils'
-import ColorPicker from '@/components/ColorPicker'
+import { shallowMount } from '@vue/test-utils';
+import ColorPicker from '@/components/ColorPicker';
+import convert from 'color-convert';
+
+let wrapper = null;
+
+const propsData = {
+  swatches: ['e3342f', '3490dc', 'f6993f', '38c172', 'fff']
+};
+
+beforeEach(() => (wrapper = shallowMount(ColorPicker, { propsData })))
+afterEach(() => wrapper.destroy())
 
 describe('ColorPicker', () => {
-  // let's do this!
+  describe('Swatches', () => {
+    test('displays each color as an individual swatch', () => {
+      const swatches = wrapper.findAll('.swatch')
+      propsData.swatches.forEach((swatch, index) => {
+        expect(swatches.at(index).attributes().style).toBe(
+          `background: rgb(${convert.hex.rgb(swatch).join(', ')});`
+        )
+      })
+    })
+  })
 })
